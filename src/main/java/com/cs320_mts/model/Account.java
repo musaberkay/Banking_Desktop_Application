9e20da57cf2d8586cdd735e768752930bbfa659a
Kerem_ <kerem.ersan@ozu.edu.tr>
2021-12-14 21:59:40 +0300

    Account constructor with 2 parameters is added and minor convention issue is fixed.

--- src/main/java/com/cs320_mts/model/Account.java
+++ src/main/java/com/cs320_mts/model/Account.java
@@ -11,7 +11,6 @@ public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int AccountId;
-    
     @Column(length = 25, nullable = false)
     private double balance;
 
