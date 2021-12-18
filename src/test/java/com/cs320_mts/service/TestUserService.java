897f02de9c5af7fe8e32ba66bbe1307628729774
Kerem_ <kerem.ersan@ozu.edu.tr>
2021-12-18 04:14:12 +0300

    TestUserService is updated.

--- src/test/java/com/cs320_mts/service/TestUserService.java
+++ src/test/java/com/cs320_mts/service/TestUserService.java
@@ -16,24 +16,43 @@ public class TestUserService
     @Autowired
     UserService userService;
 
+    /**
+     * This test creates 2 user and 2 account for each user.
+     * This method should be run in order to initialize the DB and work with it for testing purposes.
+     */
     @Test
     public void testCreateUser()
     {
 
+        // Creation of first User
         User user = new User("Kerem","Ersan",12345,"07-04-2000","35007269396","kerem.ersan@ozu.edu.tr","5057656825");
 
+        // Creation of first User's first account.
         Account account = new Account(300,user);
         user.getAccounts().add(account);
+        account.setUser(user);
 
+        // Creation of first User's second account.
         Account account2 = new Account(200,user);
         user.getAccounts().add(account2);
+        account.setUser(user);
 
-        Transaction transaction = new Transaction(new Date(),100,account.getAccountId(), account2.getAccountId());
-        account.getTransactions().add(transaction);
         userService.save(user);
 
+        // Creation of second User
+        User user2 = new User("Zuhal","Tokgözoğlu",973100,"05-06-1998","24107854293","zuhaltokgoz@gmail.com.tr","5511768127");
 
+        // Creation of second User's first account
+        Account account3 = new Account(1575,user2);
+        user2.getAccounts().add(account3);
+        account.setUser(user2);
 
+        // Creation of second User's second account
+        Account account4 = new Account(1870,user2);
+        user2.getAccounts().add(account4);
+        account4.setUser(user2);
+
+        userService.save(user2);
 
 
     }
