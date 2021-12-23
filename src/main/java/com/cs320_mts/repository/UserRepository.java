8f35ab11d9d71f5e1b3cf9cc98ed98beba3764ab
aycacevdet <80628979+aycacevdet@users.noreply.github.com>
2021-12-24 00:23:37 +0300

    changePassword method is added

--- src/main/java/com/cs320_mts/repository/UserRepository.java
+++ src/main/java/com/cs320_mts/repository/UserRepository.java
@@ -10,5 +10,11 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer>
 {
+    public static final String CHANGE_PASSWORD = "UPDATE User AS u SET u.password = :newPassword WHERE u.userId = :userId AND u.password = :oldPassword";
+    
+    @Modifying
+    @Query(value = CHANGE_PASSWORD)
+    public void changePassword(int userId, int oldPassword, int newPassword);
+    
     public User getByIdentificationNumber(String id_num);
 }
