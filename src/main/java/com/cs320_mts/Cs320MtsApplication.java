086d5c0ad6734f9e14c056215aa975cd2cc0e71d
musaberkay <musa.kocabasoglu@ozu.edu.tr>
2021-12-24 00:50:49 +0300

    Spring main class configuration made for GUI.

--- src/main/java/com/cs320_mts/Cs320MtsApplication.java
+++ src/main/java/com/cs320_mts/Cs320MtsApplication.java
@@ -1,13 +1,27 @@
 package com.cs320_mts;
 
-import org.springframework.boot.SpringApplication;
+import com.cs320_mts.GUI.Application;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 @SpringBootApplication
-public class Cs320MtsApplication {
+public class Cs320MtsApplication implements CommandLineRunner {
+
+    @Autowired
+    Application application;
 
     public static void main(String[] args) {
-        SpringApplication.run(Cs320MtsApplication.class, args);
+        new SpringApplicationBuilder(Cs320MtsApplication.class).headless(false).run(args);
+
     }
+
+    @Override
+    public void run(String... args) {
+        application.init();
+    }
+
 
 }
