fc73c87a53d5b3932d15b1af15420de7939b5f2b
aycacevdet <80628979+aycacevdet@users.noreply.github.com>
2021-12-24 00:48:15 +0300

    Update UserRepository.java

--- src/main/java/com/cs320_mts/repository/UserRepository.java
+++ src/main/java/com/cs320_mts/repository/UserRepository.java
@@ -12,9 +12,24 @@ public interface UserRepository extends JpaRepository<User,Integer>
 {
     public static final String CHANGE_PASSWORD = "UPDATE User AS u SET u.password = :newPassword WHERE u.userId = :userId AND u.password = :oldPassword";
     
+    public static final String FIND_USER_ID_NUMBERS = "SELECT u.identificationNumber FROM User AS u";
+    
+    public static final String FIND_USER_PHONE_NUMBERS = "SELECT u.phoneNumber FROM User AS u";
+    
+    public static final String FIND_USER_EMAILS = "SELECT u.email FROM User AS u";
+    
     @Modifying
     @Query(value = CHANGE_PASSWORD)
     public void changePassword(int userId, int oldPassword, int newPassword);
+    
+    @Query(value = FIND_USER_ID_NUMBERS)
+    public List<String> getIdentificationNumberList();
+    
+    @Query(value = FIND_USER_PHONE_NUMBERS)
+    public List<String> getPhoneNumberList();
+    
+    @Query(value = FIND_USER_EMAILS)
+    public List<String> getEmailList();
     
     public User getByIdentificationNumber(String id_num);
 }
