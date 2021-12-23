dd82ff67f2912e364a64d6e0f0a42247d79e4435
gokse <goksel.verep@ozu.edu.tr>
2021-12-23 15:03:18 +0300

    CheckBalance layout is handled.

--- src/main/java/com/cs320_mts/GUI/CheckBalance.java
+++ src/main/java/com/cs320_mts/GUI/CheckBalance.java
@@ -3,6 +3,7 @@ package com.cs320_mts.GUI;
 import com.cs320_mts.model.User;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 import java.awt.*;
@@ -21,17 +22,38 @@ public class CheckBalance extends JPanel {
 
         String[] columns        = {"Account ID", "Balance"};
         DefaultTableModel model = new DefaultTableModel(columns,0);
-        table            = new JTable(model);
-
+        table                   = new JTable(model);
+        model.addRow(new String[]{"Account Id","Balance"});
+        model.addRow(new String[]{"33333333333","333333333333333"});
+        model.addRow(new String[]{"12321312312","312312312312312"});
         // ************ DATABASE ACTION NECESSARY ************
         // DATABASE ACTION HERE
         // use model.addRow() with 2 String tuple For each transaction
-        // model.addRow({"5634", "3250"})
+        // model.addRow(new String[]{"5634", "3250"})
+        // model.addRow(new String[]{user.getAccounts().get(0).getAccountId(), user.getAccounts().get(0).getBalance().toString()}
         // **********************************************************************
-
         this.setSize(size);
-        this.add(back);
-        this.add(table);
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.CENTER;
+        c.gridx = 0;
+        c.gridy = 0;
+        table.setRowHeight(40);
+        c.ipadx = 500;
+        this.add(table,c);
+        c.gridx = 0;
+        c.gridy = 2;
+        c.ipadx = 200;
+        c.insets = new Insets(40,0,0,0);
+        this.add(back,c);
+
+        table.setFont(new Font("Arial",Font.ITALIC,30));
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment( JLabel.CENTER );
+        table.setDefaultRenderer(String.class, centerRenderer);
+        table.setDefaultEditor(Object.class, null);
+        back.setFont(new Font("Arial",Font.BOLD,20));
+
     }
 
     // DO NOT TOUCH THIS METHOD
